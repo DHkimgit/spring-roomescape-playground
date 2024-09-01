e2542ea585ece980a01a19fe614b1513c00ad401
DHkimgit <dukekim001@naver.com>
2024-09-01 20:56:10 +0900

    refactor : response dto를 record로 수정, 정적 생성 메서드 추가

--- src/main/java/roomescape/dto/ReservationResponseDto.java
+++ src/main/java/roomescape/dto/ReservationResponseDto.java
@@ -6,34 +6,20 @@ import roomescape.domain.Reservation;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class ReservationResponseDto {
-    private final Long id;
-    private final String name;
-    private final LocalDate date;
+public record ReservationResponseDto(
+        Long id,
+        String name,
+        LocalDate date,
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime time
+) {
 
-    @JsonFormat(pattern = "HH:mm")
-    private final LocalTime time;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public ReservationResponseDto(Reservation entity) {
-        this.id = entity.getId();
-        this.name = entity.getName();
-        this.date = entity.getDate();
-        this.time = entity.getTime();
+    public static ReservationResponseDto from(Reservation entity) {
+        return new ReservationResponseDto(
+                entity.getId(),
+                entity.getName(),
+                entity.getDate(),
+                entity.getTime()
+        );
     }
 }
