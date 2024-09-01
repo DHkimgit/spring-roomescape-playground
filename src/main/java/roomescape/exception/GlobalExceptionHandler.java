08d37c443eb3ab160165f85111c6211a50e1e845
DHkimgit <dukekim001@naver.com>
2024-09-02 00:23:50 +0900

    style : 코드 컨벤션에 따라 개행 수정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ReservationNotFoundException.class)
     public ResponseEntity<Void> handleReservationNotFound(ReservationNotFoundException e) {
         return ResponseEntity.status(HttpStatusCode.valueOf(400)).build();
