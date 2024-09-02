24f0ad1063cea1dd51dbdf6726b2f1a780097333
DHkimgit <dukekim001@naver.com>
2024-09-02 09:58:11 +0900

    refactor : 예외 처리 메서드 접근 제어자 public -> protected , final 지정

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -13,22 +13,22 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ReservationNotFoundException.class)
-    public ResponseEntity<Void> handleReservationNotFound(ReservationNotFoundException e) {
+    protected final ResponseEntity<Void> handleReservationNotFound(ReservationNotFoundException e) {
         return ResponseEntity.status(HttpStatusCode.valueOf(400)).build();
     }
 
     @ExceptionHandler(RequestMissingArgumentException.class)
-    public ResponseEntity<Void> handleRequestMissingArgument(RequestMissingArgumentException e) {
+    protected final ResponseEntity<Void> handleRequestMissingArgument(RequestMissingArgumentException e) {
         return ResponseEntity.status(HttpStatusCode.valueOf(400)).build();
     }
 
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
+    protected final ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
         return ResponseEntity.status(HttpStatusCode.valueOf(400)).build();
     }
 
     @ExceptionHandler(DateTimeParseException.class)
-    public ResponseEntity<Map<String, String>> handleDateTimeParseException(DateTimeParseException ex) {
+    protected final ResponseEntity<Map<String, String>> handleDateTimeParseException(DateTimeParseException ex) {
         Map<String, String> response = new HashMap<>();
         response.put("message", ex.getMessage());
         return ResponseEntity.status(HttpStatusCode.valueOf(400)).body(response);
